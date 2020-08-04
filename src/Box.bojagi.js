5ba9ec1ca25aa4ec0a49fb3e7d4a468152babf62
Simon Males <sime@sime.net.au>
2020-08-04 21:08:47 +0200

    First component!

---
+++ src/Box.bojagi.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import Box from './Box';
+
+export const yellow = () => <Box color="yellow">I am a yellow Box!</Box>;
+
+export const green = () => <Box color="green">I am so green!</Box>;
---
+++ src/Box.jsx
@@ -0,0 +1,12 @@
+// @bojagi
+import React from 'react';
+import styled from 'styled-components';
+
+const Container = styled.div`
+  background-color: ${({ color }) => color};
+  padding: 2rem;
+`;
+
+export default function Box(props) {
+  return <Container color={props.color}>{props.children}</Container>;
+}
