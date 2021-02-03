914abc716b6b74c799e87389878c16651ddb0e1a
Simon Males <sime@sime.net.au>
2021-02-03 15:39:23 +0100

    Introduce Button, ProgressBar and Tooltip components

---
+++ src/Button.bojagi.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Button from './Button';
+
+export default {
+  title: 'Button',
+};
+
+export const defaultBtn = () => <Button variant="contained">Default</Button>;
+export const primary = () => <Button variant="contained" color="primary">Primary</Button>;
+export const secondary = () => <Button variant="contained" color="secondary">Secondary</Button>;
+export const disabled = () => <Button variant="contained" disabled>Disabled</Button>;
