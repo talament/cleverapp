2239e19504c8e2f8fe522f9325748c4ca75cdf54
Simon Males <sime@sime.net.au>
2021-06-21 12:49:42 +0200

    Rounded corners and uppercase

--- src/Button.js
+++ src/Button.js
@@ -4,8 +4,6 @@ import { styled } from '@material-ui/core/styles';
 const Button = styled(MuiButton)(({
     theme // Pure magic from https://material-ui.com/styles/api/#examples-4
   }) => ({
-    borderRadius: 0,
-    textTransform: 'none',
     margin: theme.spacing(1),
 }));
 
